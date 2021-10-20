d5735e14dac7fdd56d1c6013f6db318624763eaa
tony-coreno <ja.coreno@edu.utc.mx>
2021-10-19 19:36:47 -0500

    try catch

--- src/Conversor/Main.java
+++ src/Conversor/Main.java
@@ -11,20 +11,28 @@ public class Main {
         float lado1;
         float lado2;
 
-        lado1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese lado1: "));
-        lado2 = Float.parseFloat((JOptionPane.showInputDialog("Ingrese lado2: ")));
+        try{
+            lado1 = Float.parseFloat(JOptionPane.showInputDialog("Ingrese lado1: "));
+            lado2 = Float.parseFloat((JOptionPane.showInputDialog("Ingrese lado2: ")));
 
 
-        if (lado1 == lado2){
-            c1 = new Conversor(lado1);
-            System.out.println("El perímetro es: "+c1.getLado1());
-            System.out.println("El área es: "+c1.getLado2());
+            if (lado1 == lado2){
+                c1 = new Conversor(lado1);
+                System.out.println("El perímetro es: "+c1.getLado1());
+                System.out.println("El área es: "+c1.getLado2());
 
-        }
-        else {
-            c1 = new Conversor(lado1, lado2);
-            System.out.println("El perímetro es: "+c1.getLado1());
-            System.out.println("El área es: "+c1.getLado2());
+            }
+            else {
+                c1 = new Conversor(lado1, lado2);
+                System.out.println("El perímetro es: "+c1.getLado1());
+                System.out.println("El área es: "+c1.getLado2());
+            }
+
+
+
+
+        }catch (Exception e){
+            System.out.println(e);
         }
 
 
